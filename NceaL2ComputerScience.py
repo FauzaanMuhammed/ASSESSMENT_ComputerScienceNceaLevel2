8656cdd13b016588c4d7fad010c55613d9b3f592
Fauzaan <fauzaan.muhammed@outlook.com>
2023-05-08 14:37:17 +1200

    Added a placeholder for where the image will be

--- NceaL2ComputerScience.py
+++ NceaL2ComputerScience.py
@@ -2,6 +2,8 @@ import tkinter as tk
 from tkinter import *
 import time
 root = tk.Tk()
+image_placeholder = Label(root,text="Image")
+image_placeholder.grid(column=0,row=0)
 
 root.geometry("1200x800-40+0")
 root.mainloop()
