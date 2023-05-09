f20e36f806876402954f178be5d5da9d20c9850a
Fauzaan <fauzaan.muhammed@outlook.com>
2023-05-09 16:19:55 +1200

    Added title text with 20px font size

--- NceaL2ComputerScience.py
+++ NceaL2ComputerScience.py
@@ -5,5 +5,8 @@ root = tk.Tk()
 image_placeholder = Label(root,text="Image")
 image_placeholder.grid(column=0,row=0)
 
+title_label = Label(root,text="Julie’s Party Hire",font=(("Arial"),20))
+title_label.place(x=475,y=0)
+
 root.geometry("1200x800-40+0")
 root.mainloop()
